97749a74dfe23a434559be54ac3a422e1e4c4dae
Rahul <ajrahul580@gmail.com>
2025-04-11 15:12:08 +0530

    Fix: updated Vite config to use 0.0.0.0 for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // Optional: You can use any port or let Render assign it
+  },
 })
